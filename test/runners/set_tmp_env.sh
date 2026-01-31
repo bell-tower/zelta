8b99afc95fd4a90b957d810eeebd22bf4bf80536
Richard Logwood <richard.logwood@gmail.com>
2026-01-31 12:37:47 -0500

    use more portable, ls -1d to find last tmp zelta install

--- test/runners/set_tmp_env.sh
+++ test/runners/set_tmp_env.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -x
 # to facilitate creating shellspec tests, use the last spec installed zelta version
-last_tmp_installed_zelta_ver=$(ls -ld /tmp/zelta* | tail -1 | cut -d ' ' -f 9)
+#last_tmp_installed_zelta_ver=$(ls -ld /tmp/zelta* | tail -1 | cut -w -f 9)
+last_tmp_installed_zelta_ver=$(ls -1d /tmp/zelta* | tail -1)
 last_tmp_process_number=$(echo "$last_tmp_installed_zelta_ver" | grep -o '[0-9]\+$')
 export SANDBOX_ZELTA_TMP_DIR="$last_tmp_installed_zelta_ver"
 export SANDBOX_ZELTA_PROCNUM="$last_tmp_process_number"
